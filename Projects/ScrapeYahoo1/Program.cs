b32f33c7e68b75443090f55a6917f3eba063aa46
Stephanie <stephcshea@gmail.com>
2018-06-24 17:25:26 -0400

    successful logging in and navigating to portfolio

--- Projects/ScrapeYahoo1/Program.cs
+++ Projects/ScrapeYahoo1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -19,7 +21,7 @@ namespace ScrapeYahoo1
             var chromeDriver = new ChromeDriver();
 
             var wait = new WebDriverWait(chromeDriver, new TimeSpan(0, 0, 30));
-            chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F/");
+            chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F");
             //chromeDriver.FindElementByXPath("//*[@id='login - username']");
 
              var userName = chromeDriver.FindElementById("login-username");
@@ -34,14 +36,23 @@ userPassword.SendKeys("Abby7777!");
 var loginButton2 = chromeDriver.FindElementById("login-signin");
 loginButton2.Click();
 
- var profileTable = chromeDriver.FindElementByClassName("Mb(20px)");
- //*[@id="data-util-col"]/section[2]
+chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(300);
 
-            Console.WriteLine(profileTable);
+chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v1");
 
-//*[@id="data-util-col"]/section[2]
-//*[@id="data-util-col"]/section[2]/table
-     //*[@id="data-util-col"]/section[2]      
+var popup = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("__dialog")));  
+
+var closePopup = chromeDriver.FindElementByXPath("//dialog[@id = '__dialog']/section/button");
+closePopup.Click();
+
+
+// var profileTable = chromeDriver.FindElementByXPath("//[@id='data-util-col']/section[2]/table"); 
+
+// var profileTable = chromeDriver.FindElementByXPath("//*[@id=\'main\']/section/section[2]/div[2]").Text; 
+
+var profileTable = chromeDriver.FindElementByXPath("//[@id=\'main\']/section/section._64nqq/div.gIc8M/table").Text;
+
+System.Console.WriteLine("profileTable: ", profileTable);  
         }
     }
 }
