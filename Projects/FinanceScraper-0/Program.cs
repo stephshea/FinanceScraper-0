6bded989313036240c01d5bc3324a5902360e832
Stephanie <stephcshea@gmail.com>
2018-06-25 20:57:32 -0400

    changed from chromedriver to firefox

--- Projects/FinanceScraper-0/Program.cs
+++ Projects/FinanceScraper-0/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Net;
 using System.Collections.Generic;
 using System.IO;
@@ -9,6 +11,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Firefox; 
 
 namespace FinanceScraper0
 {    
@@ -16,42 +19,46 @@ namespace FinanceScraper0
     {
         public static void Main(string[] args)
         {
-            //  var options = new ChromeOptions();
-            //  options.AddArguments("--disable-gpu");
+    //    FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/Users/Owner/Projects/FinanceScraper-0/bin/Debug/netcoreapp2.1/");
 
-            var chromeDriver = new ChromeDriver();
+           var driver = new FirefoxDriver();
+        //    ("/Users/Owner/Projects/FinanceScraper-0/bin/Debug/netcoreapp2.1/");
 
-            var wait = new WebDriverWait(chromeDriver, new TimeSpan(0, 0, 30));
-            chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F");
-            //chromeDriver.FindElementByXPath("//*[@id='login - username']");
+            // var FirefoxDriver = new FirefoxDriver();
 
-             var userName = chromeDriver.FindElementById("login-username");
+            // var wait = new WebDriverWait(FirefoxDriver, new TimeSpan(0, 0, 30));
+
+            driver.Url = "https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F";
+
+             var userName = driver.FindElementById("login-username");
             userName.SendKeys("stephcshea");
-            var loginButton = chromeDriver.FindElementById("login-signin");
+            var loginButton = driver.FindElementById("login-signin");
             loginButton.Click();
 
-      var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("login-passwd")));       
+    (SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("login-passwd")));       
 
-var userPassword = chromeDriver.FindElementById("login-passwd");
+var userPassword = driver.FindElementById("login-passwd");
 userPassword.SendKeys("Abby7777!");
-var loginButton2 = chromeDriver.FindElementById("login-signin");
+var loginButton2 = driver.FindElementById("login-signin");
 loginButton2.Click();
 
-chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
+//redirect
 
-chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v1");
+// FirefoxDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
 
-var popup = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("__dialog")));  
+// FirefoxDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v2");
 
-var closePopup = chromeDriver.FindElementByXPath("//dialog[@id = '__dialog']/section/button");
-closePopup.Click(); 
+// var popup = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("__dialog")));  
+
+// var closePopup = FirefoxDriver.FindElementByXPath("//dialog[@id = '__dialog']/section/button");
+// closePopup.Click(); 
 
 
-var profile = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("main")));
+// var profile = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("main")));
 
-var profileTable = chromeDriver.FindElement(By.XPath("//*[@id=\"main\"]/section/header/div/div[1]/div/div[2]/p[1]")).Text;
+// var profileTable = FirefoxDriver.FindElement(By.XPath("//*[@id=\"main\"]/section/header/div/div[2]/div/div[3]/section/section[2]/div/div[2]")).Text;
 
-System.Console.WriteLine("profileTable: ", profileTable);  
+// System.Console.WriteLine("profileTable: ", profileTable);  
         }
     }
 }
